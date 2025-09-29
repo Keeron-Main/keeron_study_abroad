d7d3b965ae1ca064dc2e1b4f3efa9117cf607486
Sakibul Islam <sakibul95bubt@gmail.com>
2025-09-29 17:03:33 +0600

    default theme and country wise content updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       */}
       <head />
 
+      {/* <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}> */}
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
           <Header />
