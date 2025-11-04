3d9b70825cd9c029421d705d026f90ac13ffdaf4
Sakibul <sakibul95bubt@gmail.com>
2025-11-04 23:54:06 +0600

    issue resolved

--- src/components/Contact/NewsLatterBox.tsx
+++ src/components/Contact/NewsLatterBox.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 
 const NewsLatterBox = () => {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
 
   return (
     <div className="shadow-three dark:bg-gray-dark relative z-10 rounded-xs bg-white p-8 sm:p-11 lg:p-8 xl:p-11">
@@ -61,12 +61,12 @@ const NewsLatterBox = () => {
                 gradientUnits="userSpaceOnUse"
               >
                 <stop
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0.62"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
@@ -97,12 +97,12 @@ const NewsLatterBox = () => {
                 gradientUnits="userSpaceOnUse"
               >
                 <stop
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0.62"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
@@ -133,12 +133,12 @@ const NewsLatterBox = () => {
                 gradientUnits="userSpaceOnUse"
               >
                 <stop
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0.62"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
@@ -187,11 +187,11 @@ const NewsLatterBox = () => {
               >
                 <stop
                   offset="0.328125"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
@@ -205,11 +205,11 @@ const NewsLatterBox = () => {
               >
                 <stop
                   offset="0.328125"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
@@ -223,11 +223,11 @@ const NewsLatterBox = () => {
               >
                 <stop
                   offset="0.328125"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
@@ -241,11 +241,11 @@ const NewsLatterBox = () => {
               >
                 <stop
                   offset="0.328125"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                 />
                 <stop
                   offset="1"
-                  stopColor={theme === "light" ? "#4A6CF7" : "#fff"}
+                  stopColor="#4A6CF7"
                   stopOpacity="0"
                 />
               </linearGradient>
