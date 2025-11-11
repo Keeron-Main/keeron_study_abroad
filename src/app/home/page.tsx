ffa5bfd6d16f21dfb8515ad2bb9436b1d42b24ed
Mahabubur Rahman <mahabub.prog@gmail.com>
2025-11-12 00:45:45 +0600

    menu and home page path update

---
+++ src/app/home/page.tsx
@@ -0,0 +1,44 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import HomeCourseSelection from "@/components/Home/HomeCourseSelection";
+import HomeFaq from "@/components/Home/HomeFaq";
+import HomeScolarship from "@/components/Home/HomeScolarship";
+import HomeSOP from "@/components/Home/HomeSOP";
+import Pricing from "@/components/Pricing";
+import Testimonials from "@/components/Testimonials";
+import Video from "@/components/Video";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Study Abroad with Keeron",
+  description: "Study Abroad with Keeron",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Video />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <HomeScolarship />
+      <HomeSOP />
+      <HomeCourseSelection />
+      <HomeFaq />
+      <Testimonials />
+      {/* <Pricing /> */}
+      {/* <Blog /> */}
+      <Contact />
+    </>
+  );
+}
